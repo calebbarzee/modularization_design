2a4a4184ca17e3e16ebe9df77e5ee219b930eb30
Caleb Barzee <barzeec@gmail.com>
2022-04-23 13:47:21 -0600

    tic tac toe 2nd commit

--- tic_tac_toe/Lab01.py
+++ tic_tac_toe/Lab01.py
@@ -8,6 +8,7 @@
 #      -a paragraph or two about how the assignment went for you-
 # 5. How long did it take for you to complete the assignment?
 #      -total time in hours including reading the assignment and submitting the program-
+import os
 import json
 import constants
 
@@ -17,61 +18,74 @@ def initialize_blank_board():
         "board": [
             constants.BLANK, constants.BLANK, constants.BLANK,
             constants.BLANK, constants.BLANK, constants.BLANK,
-            constants.BLANK, constants.BLANK, constants.BLANK]
+            constants.BLANK, constants.BLANK, constants.BLANK],
+        "turn_count": 0
     }
     return board_state
 
 
 def read_board():
-    '''Read the previously existing board from the file if it exists.'''
-    with open("tic_tac_toe/board.json") as board_file:
+    """Read the previously existing board from the file if it exists."""
+
+    with open("tic_tac_toe/board.json", "r") as board_file:
+        print(board_file.read())
         board_state = json.loads(board_file.read())
 
     return board_state
 
 
-def save_board(board):
-    '''Save the current game to a file.'''
-    with open("tic_tac_toe/board.json") as board_file:
-        board_file.write(json.dumps(board_state['board']))
+def save_board(board_state):
+    """Save the current game to a file."""
+    with open("tic_tac_toe/board.json", "w") as board_file:
+        board_file.write(json.dumps(board_state))
 
 
 def display_board(board):
-    '''Display a Tic-Tac-Toe board on the screen in a user-friendly way.'''
+    """Display a Tic-Tac-Toe board on the screen in a user-friendly way."""
     print(
-        f" {board_state['board'][0]} | {board_state['board'][1]} | {board_state['board'][2]} ")
+        f" {board[0]} | {board[1]} | {board[2]} ")
     print("---+---+---")
     print(
-        f" {board_state['board'][3]} | {board_state['board'][4]} | {board_state['board'][5]} ")
+        f" {board[3]} | {board[4]} | {board[5]} ")
     print("---+---+---")
     print(
-        f" {board_state['board'][6]} | {board_state['board'][7]} | {board_state['board'][8]} \n")
+        f" {board[6]} | {board[7]} | {board[8]} \n")
 
 
-def is_x_turn(board):
-    '''Determine whose turn it is.'''
-    # Put code here determining if it is X's turn.
-    return True
+def determine_turn(turn_count):
+    """Determine whose turn it is."""
+    if turn_count % 2 == 0:
+        return "O"
+    else:
+        return "X"
 
 
-def play_game(board):
-    '''Play the game of Tic-Tac-Toe.'''
+def play_game(board_state):
+    """Play the game of Tic-Tac-Toe."""
     # Put game play code here. Return False when the user has indicated they are done.
-    print("The current board is:")
-    display_board(board)
-    response = input()
-    if response == "q":
-        return False
+    print(board_state)
+    while True:
+        print("The current board is:")
+        current_board = board_state["board"]
+        display_board(current_board)
+        board_state.turn_count += 1
+        response = input()
+        if response == "q":
+            return board_state
+        board_state["board"][response] = determine_turn(board_state.turn_count)
+        if not game_done(board_state["board"]):
+            initialize_blank_board()
+            return board_state
 
 # Why would we ever desire to hide the results from the user?
 
 
 def game_done(board, message=True):
-    '''Determine if the game is finished.
+    """Determine if the game is finished.
        Note that this function is provided as-is.
        You do not need to edit it in any way.
        If message == True, then we display a message to the user.
-       Otherwise, no message is displayed. '''
+       Otherwise, no message is displayed. """
 
     # Game is finished if someone has completed a row.
     for row in range(3):
@@ -108,12 +122,13 @@ def game_done(board, message=True):
 
 
 def main():
-    # A blank Tic-Tac-Toe board. We should not need to change this board;
-    # it is only used to reset the board to blank. This should be the format
-    # of the code in the JSON file.
+    # checking file path
+    # directory = os.listdir("tic_tac_toe")
+    # print(f"file path {directory}")
     board_state = read_board()
 
     # These user-instructions are provided and do not need to be changed.
+    print("X goes first!")
     print("Enter 'q' to suspend your game. Otherwise, enter a number from 1 to 9")
     print("where the following numbers correspond to the locations on the grid:")
     print(" 1 | 2 | 3 ")
@@ -122,8 +137,9 @@ def main():
     print("---+---+---")
     print(" 7 | 8 | 9 \n")
 
-    while not game_done(board_state["board"]):
-        q_response = play_game(board_state["board"])
-        if not q_response:
-            save_board(board_state)
-            return
+    board_state = play_game(board_state)
+    save_board(board_state)
+
+
+if __name__ == "__main__":
+    main()
